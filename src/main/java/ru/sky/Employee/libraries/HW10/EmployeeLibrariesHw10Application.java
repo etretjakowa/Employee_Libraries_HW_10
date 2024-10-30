3ae8e791db560ccc8bc2cbc09013df6c3509bc4e
etretjakova <etretjakowa@yandex.ru>
2024-10-30 21:25:48 +0300

    initial commit

---
+++ src/main/java/ru/sky/Employee/libraries/HW10/EmployeeLibrariesHw10Application.java
@@ -0,0 +1,13 @@
+package ru.sky.Employee.libraries.HW10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLibrariesHw10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeLibrariesHw10Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sky/Employee/libraries/HW10/EmployeeLibrariesHw10ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sky.Employee.libraries.HW10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeLibrariesHw10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
